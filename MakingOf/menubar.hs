743c2533b523783940574408e7a3fc9ba1237818
jsavatgy <jsavatgy@users.noreply.github.com>
2014-11-01 13:58:34 +0200

    menubar #1

---
+++ MakingOf/menubar.hs
@@ -0,0 +1,45 @@
+module Main (main) where
+
+import Graphics.UI.Gtk
+
+createMenuBar descr = do 
+  bar <- menuBarNew
+  mapM_ (createMenu bar) descr
+  return bar
+  where
+    createMenu bar (name,items) = do 
+      menu <- menuNew
+      item <- menuItemNewWithLabelOrMnemonic name
+      menuItemSetSubmenu item menu
+      menuShellAppend bar item
+      mapM_ (createMenuItem menu) items
+    createMenuItem menu (stock,action) = do 
+      item <- imageMenuItemNewFromStock stock
+      menuShellAppend menu item
+      onActivateLeaf item action
+    menuItemNewWithLabelOrMnemonic name
+      | elem '_' name = menuItemNewWithMnemonic name
+      | otherwise     = menuItemNewWithLabel name
+
+noop = do
+  return ()
+
+menuBarDescr = 
+  [("_Tiedosto", 
+    [("gtk-open", noop),
+     ("gtk-select-font", noop),
+     ("gtk-preferences", noop),
+     ("gtk-about", noop),
+     ("gtk-quit", mainQuit)])
+  ]
+
+main = do 
+  initGUI
+  window <- windowNew
+  menuBar <- createMenuBar menuBarDescr
+  set window [ windowTitle := "Manual Menubar Demo",
+               containerChild := menuBar ]
+  onDestroy window mainQuit
+  widgetShowAll window
+  mainGUI
+
